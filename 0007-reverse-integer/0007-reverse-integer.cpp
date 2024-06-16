6a8ed10a4e78dff2b922453b114da817c4be561b
Rahul-Encoded <122997975+Rahul-Encoded@users.noreply.github.com>
2024-06-16 13:35:51 +0530

    Time: 2 ms (39.17%), Space: 7.5 MB (52.1%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rem;
+        int rev = 0;
+
+        while (x!=0){
+            rem = (x % 10);
+        
+            if (rev < INT_MIN/10 || rev > INT_MAX/10){
+                return 0;
+            }
+
+            rev = 10*rev + rem;
+            x = x/10;
+        }
+
+        return rev;   
+    }
+};
