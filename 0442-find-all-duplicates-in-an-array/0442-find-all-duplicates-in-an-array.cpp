3611cae06fa02372c91268d2ae35a34be6ca755a
Rahul-Encoded <122997975+Rahul-Encoded@users.noreply.github.com>
2024-06-16 18:21:36 +0530

    Time: 68 ms (24.57%), Space: 48 MB (28.75%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        unordered_map<int, int> hash;
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++){
+            hash[nums[i]]++;
+        }
+
+        for(auto it: hash){
+            if(it.second > 1){
+                ans.push_back(it.first);
+            }
+        }
+
+        return ans;
+    }
+};
