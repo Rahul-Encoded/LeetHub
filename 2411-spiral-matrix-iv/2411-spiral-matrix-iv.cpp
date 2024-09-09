3526f6e6c6876a4ec40427343d8d1fc79e172797
Rahul-Encoded <122997975+Rahul-Encoded@users.noreply.github.com>
2024-09-09 20:15:36 +0530

    Time: 148 ms (91.94%), Space: 130.7 MB (6.22%) - LeetHub

---
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> matrix(m, vector<int>(n, -1));
+        
+        int top = 0, bottom = m - 1, left = 0, right = n - 1;
+        
+        ListNode* current = head;
+        
+        while (top <= bottom && left <= right && current != nullptr) {
+
+            for (int j = left; j <= right && current != nullptr; j++) {
+                matrix[top][j] = current->val;
+                current = current->next;
+            }
+            top++;
+            
+
+            for (int i = top; i <= bottom && current != nullptr; i++) {
+                matrix[i][right] = current->val;
+                current = current->next;
+            }
+            right--;
+            
+
+            for (int j = right; j >= left && current != nullptr; j--) {
+                matrix[bottom][j] = current->val;
+                current = current->next;
+            }
+            bottom--;
+            
+
+            for (int i = bottom; i >= top && current != nullptr; i--) {
+                matrix[i][left] = current->val;
+                current = current->next;
+            }
+            left++;
+        }
+        
+        return matrix;
+    }
+};
