3e03ba4f8909198e99eaffb9d994650460474e4b
Rahul-Encoded <122997975+Rahul-Encoded@users.noreply.github.com>
2024-09-13 19:21:04 +0530

    Time: 57 ms (71.47%), Space: 42.2 MB (42.84%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -6,14 +6,36 @@ public:
         int left;
         int right;
 
+        // for(int i = 0; i < queries.size(); i++){
+        //     left = queries[i][0];
+        //     right = queries[i][1];
+        //     int xorSum = 0;
+        //     for(int j = left; j <= right; j++){
+        //         xorSum ^= arr[j];
+        //     }
+        //     ans.push_back(xorSum);
+        // }
+
+        vector<int> prefixXor(arr.size());
+        prefixXor[0] = arr[0];
+
+        for(int i = 1; i < arr.size(); i++){
+            prefixXor[i] = prefixXor[i-1] ^ arr[i];
+        }
+
         for(int i = 0; i < queries.size(); i++){
             left = queries[i][0];
             right = queries[i][1];
-            int xorSum = 0;
-            for(int j = left; j <= right; j++){
-                xorSum ^= arr[j];
+
+            if(left == 0){
+                int result = prefixXor[right];
+                ans.push_back(result);
             }
-            ans.push_back(xorSum);
+            else{
+                int result = prefixXor[left - 1] ^ prefixXor[right];
+                ans.push_back(result);  
+            }
+
         }
 
         return ans;
