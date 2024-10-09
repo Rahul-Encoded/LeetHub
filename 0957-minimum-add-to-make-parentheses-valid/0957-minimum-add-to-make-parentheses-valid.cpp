bcba238ae414d003ac7f3089e1a1cde143881662
Rahul-Encoded <122997975+Rahul-Encoded@users.noreply.github.com>
2024-10-09 20:20:44 +0530

    Time: 0 ms (100%), Space: 7.8 MB (55.34%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char> st;
+        // stack<char> st;
+        int open = 0;
+        int close = 0;
         for(int i = 0; i < s.length(); i++){  
-            if(!st.empty() && s[i] == ')' && st.top() == '('){  
-                st.pop();
+            // if(!st.empty() && s[i] == ')' && st.top() == '('){  
+            //     st.pop();
+            // }
+            // else{
+            //     st.push(s[i]);
+            // }
+
+            if(s[i] == '('){
+                open++;
             }
             else{
-                st.push(s[i]);
+                if(open > 0){
+                    open--;
+                }
+                else{
+                    close++;
+                }
             }
+
         }
-        return st.size();
+        // return st.size();
+        return (close + open);
     }
 };
