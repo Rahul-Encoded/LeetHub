11be6b22c8708abe03ab5bd8a71e36d34719b786
Rahul <122997975+Rahul-Encoded@users.noreply.github.com>
2025-11-19 13:26:28 +0530

    Time: 0 ms (100%), Space: 9.3 MB (78.9%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,33 +2,40 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int n = matrix.size();
+        int m = matrix[0].size();
+        int ele = n*m;
+        int count = 0;
 
-        int right = n-1;
+        int right = m-1;
         int bottom = n-1;
         int top = 0;
         int left = 0;
-        int k = n-1;
+        
 
         vector<int> arr;
 
-        while(k){
-            for(int i = left; i <= right; i++){
+        while(count < ele){
+            for(int i = left; i <= right && count < ele; i++){
                 arr.push_back(matrix[top][i]);
+                count++;
+            }
                 top++;
+            for(int i = top; i <= bottom && count < ele; i++){
+                arr.push_back(matrix[i][right]);
+                count++;
             }
-            for(int i = top; i <= bottom; i++){
-                arr.push_back(matrix[i][right])
                 right--;
-            }
-            for(int i = right; i >= left; i--){
+            for(int i = right; i >= left && count < ele; i--){
                 arr.push_back(matrix[bottom][i]);
+                count++;
+            }
                 bottom--;
-            }
-            for(int i = bottom; i >= top; i--){
+            for(int i = bottom; i >= top && count < ele; i--){
                 arr.push_back(matrix[i][left]);
-                left++
+                count++;
             }
-            k--;
+                left++;
+            
         }
         return arr;
     }
